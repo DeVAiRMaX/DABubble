a4d0d19a94a29b96fb796497f43677695c2223fd
Maximilian Wagener <159955254+DeVAiRMaX@users.noreply.github.com>
2025-03-16 14:11:05 +0100

    #8

--- src/app/shared/side-nav/side-nav.component.ts
+++ src/app/shared/side-nav/side-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../shared';
 
 @Component({
@@ -8,6 +8,28 @@ import { SharedModule } from '../../shared';
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.scss'
 })
-export class SideNavComponent {
- 
+export class SideNavComponent implements OnInit {
+  isChannelListExpanded: boolean = true;
+  isMsgListExpanded: boolean = true;
+
+  ngOnInit() {
+    // Lade gespeicherte Zustände aus dem LocalStorage
+    const savedChannelState = localStorage.getItem('channelListExpanded');
+    const savedMsgState = localStorage.getItem('msgListExpanded');
+    
+    this.isChannelListExpanded = savedChannelState ? JSON.parse(savedChannelState) : true;
+    this.isMsgListExpanded = savedMsgState ? JSON.parse(savedMsgState) : true;
+  }
+
+  toggleChannelList() {
+    this.isChannelListExpanded = !this.isChannelListExpanded;
+    // Speichere neuen Zustand im LocalStorage
+    localStorage.setItem('channelListExpanded', JSON.stringify(this.isChannelListExpanded));
+  }
+
+  toggleMsgList() {
+    this.isMsgListExpanded = !this.isMsgListExpanded;
+    // Speichere neuen Zustand im LocalStorage
+    localStorage.setItem('msgListExpanded', JSON.stringify(this.isMsgListExpanded));
+  }
 }
