11b1a7425fda141e85dac1ae40a3b39f957306d7
Maximilian Wagener <159955254+DeVAiRMaX@users.noreply.github.com>
2025-03-20 13:30:01 +0100

    #profil-dialog animation

--- src/app/shared/header-dialog-profil/dialog.component.ts
+++ src/app/shared/header-dialog-profil/dialog.component.ts
@@ -3,14 +3,17 @@ import { SharedModule } from './../../shared';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ProfilDialogComponent } from '../user-profil/profil-dialog/profil-dialog.component';
 
+
 @Component({
   selector: 'app-dialog',
   standalone: true,
   imports: [SharedModule],
   templateUrl: './dialog.component.html',
-  styleUrl: './dialog.component.scss'
+  styleUrl: './dialog.component.scss',
+  
 })
 export class DialogComponent {
+
   constructor(public dialog: MatDialog, public dialogRef: MatDialogRef<DialogComponent>) {
   }
 
--- src/app/shared/user-profil/profil-dialog/profil-dialog.component.ts
+++ src/app/shared/user-profil/profil-dialog/profil-dialog.component.ts
@@ -1,18 +1,63 @@
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  AnimationEvent
+} from '@angular/animations';
 
 @Component({
   selector: 'app-profil-dialog',
   standalone: true,
   imports: [],
   templateUrl: './profil-dialog.component.html',
-  styleUrl: './profil-dialog.component.scss'
+  styleUrl: './profil-dialog.component.scss',
+  animations: [
+    trigger('animationstate', [
+      state('open', style({
+        transform: 'translateX(0)',
+        opacity: 1
+      })),
+      state('close', style({
+        transform: 'translateX(100%)',
+        opacity: 0
+      })),
+      transition('close => open', [
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate('0.25s ease-out')
+      ]),
+      transition('open => close', [
+        animate('0.25s ease-out',
+          style({ transform: 'translateX(100%)', opacity: 0 })
+        )
+      ])
+    ])
+  ]
 })
 export class ProfilDialogComponent {
+
+  animationstate: 'open' | 'close' = 'close';
+
   constructor(public dialog: MatDialog, public dialogRef: MatDialogRef<ProfilDialogComponent>) {
   }
 
+  ngOnInit() {
+    this.startAnimation();
+  }
+
+  startAnimation() {
+    setTimeout(() => {
+      this.animationstate = 'open';
+    }, 10);
+  }
+
   closeDialog() {
-    this.dialogRef.close(ProfilDialogComponent);
+    this.animationstate = 'close';
+    setTimeout(() => {
+      this.dialogRef.close(ProfilDialogComponent);
+    }, 250);
   }
 }
