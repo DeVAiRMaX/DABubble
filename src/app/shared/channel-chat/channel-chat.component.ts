3cde4d907f169334485bc29db9192b2171c6f1f6
Maximilian Wagener <159955254+DeVAiRMaX@users.noreply.github.com>
2025-03-20 18:49:53 +0100

    #edit-channel-dialog

--- src/app/shared/channel-chat/channel-chat.component.ts
+++ src/app/shared/channel-chat/channel-chat.component.ts
@@ -1,22 +1,31 @@
 import { Component } from '@angular/core';
 import { VariablesService } from '../../variables.service';
+import { MatDialog } from '@angular/material/dialog';
+import { SharedModule } from '../../shared';
+import { EditChannelComponent } from './edit-channel/edit-channel.component';
 
 @Component({
   selector: 'app-channel-chat',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './channel-chat.component.html',
   styleUrl: './channel-chat.component.scss'
 })
 export class ChannelChatComponent {
 
-  constructor(private variableService: VariablesService){}
+  constructor(private variableService: VariablesService, public dialog: MatDialog) { }
 
   toggleThread() {
-    if (this.variableService['isClosedSubject'].value) { 
+    if (this.variableService['isClosedSubject'].value) {
       this.variableService.toggleThread();
     }
   }
-  
+
+  openEditChannelDialog() {
+    this.dialog.open(EditChannelComponent, {
+      maxWidth: 'none',
+      panelClass: 'custom-dialog-container',
+    });
+  }
 
 }
