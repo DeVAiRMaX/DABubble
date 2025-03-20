7fa47d2e174b5873f758c01d578ead7706655cd1
Maximilian Wagener <159955254+DeVAiRMaX@users.noreply.github.com>
2025-03-20 20:16:48 +0100

    #edits

--- src/app/shared/channel-create-overlay/channel-create-overlay.component.ts
+++ src/app/shared/channel-create-overlay/channel-create-overlay.component.ts
@@ -1,38 +1,61 @@
 import { Component } from '@angular/core';
 import { VariablesService } from '../../variables.service';
-import { trigger, transition, style, animate } from '@angular/animations';
+import { trigger, transition, style, animate, state } from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDialogRef } from '@angular/material/dialog';
+import { SharedModule } from '../../shared';
 
 @Component({
   selector: 'app-channel-create-overlay',
   standalone: true,
-  imports: [CommonModule, MatDialogModule],
+  imports: [CommonModule, MatDialogModule, SharedModule],
   templateUrl: './channel-create-overlay.component.html',
   styleUrl: './channel-create-overlay.component.scss',
   animations: [
-    trigger('fadeSlideIn', [
-     
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('300ms ease-out', style({ opacity: 1 }))]) ,
-
-     
-      transition(':leave', [
-        style({opacity: 1}),
-        animate('300ms ease-in', style({ opacity: 0 })) 
+    trigger('channelCreateOverlayAnimation', [
+      state('open', style({
+        transform: 'scale(1)',
+        opacity: 1,
+      })),
+      state('close', style({
+        transform: 'scale(0)',
+        opacity: 0,
+      })),
+      transition('close => open', [
+        style({ transform: 'scale(0)', opacity: 0 }),
+        animate('0.2s ease-out')
+      ]),
+      transition('open => close', [
+        animate('0.2s ease-out',
+          style({ transform: 'scale(0)', opacity: 0 })
+        )
       ])
     ])
   ]
 })
 export class ChannelCreateOverlayComponent {
 
+  channelCreateOverlayAnimation: 'open' | 'close' = 'close';
+
   constructor(private variableService: VariablesService, private dialogRef: MatDialogRef<ChannelCreateOverlayComponent>){
   }
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.startAnimation();
+    }, 10);
+  }
+
+  startAnimation() {
+    this.channelCreateOverlayAnimation = 'open';
+  }
+
   closeDialog(){
-    this.dialogRef.close();
+    this.channelCreateOverlayAnimation = 'close';
+    setTimeout(() => {
+      this.dialogRef.close(ChannelCreateOverlayComponent);
+    }, 100);
   }
 
   closeAddChannelOverlay(){
