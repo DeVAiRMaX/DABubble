94be61089992e697961a01a553e1ea902f998252
GiovanniWitulski <witulski.schule@gmail.com>
2025-03-25 17:25:44 +0100

    added search funktions for channels in the firebase

--- src/app/shared/channel-chat/channel-chat.component.ts
+++ src/app/shared/channel-chat/channel-chat.component.ts
@@ -5,73 +5,81 @@ import { SharedModule } from '../../shared';
 import { EditChannelComponent } from './edit-channel/edit-channel.component';
 import { AddUserToChannelOverlayComponent } from './add-user-to-channel-overlay/add-user-to-channel-overlay.component';
 import { CommonModule } from '@angular/common';
-import { MatDialog, MatDialogConfig, MatDialogModule } from '@angular/material/dialog';
+import {
+  MatDialog,
+  MatDialogConfig,
+  MatDialogModule,
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-channel-chat',
   standalone: true,
-  imports: [AddUserToChannelOverlayComponent, CommonModule, MatDialogModule, SharedModule],
+  imports: [
+    AddUserToChannelOverlayComponent,
+    CommonModule,
+    MatDialogModule,
+    SharedModule,
+  ],
   templateUrl: './channel-chat.component.html',
-  styleUrl: './channel-chat.component.scss'
+  styleUrl: './channel-chat.component.scss',
 })
 export class ChannelChatComponent {
-  addUserToChannelOverlayIsVisible:boolean = false;
-  
-  constructor(private variableService: VariablesService, private dialog: MatDialog){
-    
-    this.variableService.addUserToChannelOverlayIsVisible$.subscribe((value) => {
-      this.addUserToChannelOverlayIsVisible = value;
-    });
+  addUserToChannelOverlayIsVisible: boolean = false;
+
+  constructor(
+    private variableService: VariablesService,
+    private dialog: MatDialog
+  ) {
+    this.variableService.addUserToChannelOverlayIsVisible$.subscribe(
+      (value) => {
+        this.addUserToChannelOverlayIsVisible = value;
+      }
+    );
   }
-  
-  toggleAddUserToChannelOverlay(){
+
+  toggleAddUserToChannelOverlay() {
     this.variableService.toggleAddUserToChannelOverlay();
     console.log(this.addUserToChannelOverlayIsVisible);
   }
-  
+
   toggleThread() {
     if (this.variableService['isClosedSubject'].value) {
       this.variableService.toggleThread();
     }
   }
-  
-  
+
   openEditChannelDialog() {
     this.dialog.open(EditChannelComponent, {
       maxWidth: 'none',
       panelClass: 'custom-dialog-container',
     });
   }
-  
+
   openAddUserToChannelDialog() {
     const targetElement = document.querySelector('.add_btn_user');
-    
+
     if (targetElement) {
       const rect = targetElement.getBoundingClientRect(); // Position des Buttons ermitteln
-      
+
       const dialogRef = this.dialog.open(AddUserToChannelOverlayComponent, {
-        position: { top: `${rect.bottom + 20 + window.scrollY}px` }, 
-        panelClass: 'custom-dialog',// 20px Abstand nach unten
+        position: { top: `${rect.bottom + 20 + window.scrollY}px` },
+        panelClass: 'custom-dialog', // 20px Abstand nach unten
       });
-      
-      
+
       setTimeout(() => {
-        const dialogElement = document.querySelector('mat-dialog-container') as HTMLElement;
+        const dialogElement = document.querySelector(
+          'mat-dialog-container'
+        ) as HTMLElement;
         if (dialogElement) {
           const dialogRect = dialogElement.getBoundingClientRect();
-          const newLeft = rect.right - dialogRect.width + window.scrollX; 
-          
+          const newLeft = rect.right - dialogRect.width + window.scrollX;
+
           dialogElement.style.left = `${newLeft}px`;
-          dialogElement.style.position = 'absolute'; 
+          dialogElement.style.position = 'absolute';
           dialogElement.style.maxWidth = '515px';
           dialogElement.style.maxHeight = '295px';
         }
-      }, 0); 
+      }, 0);
     }
   }
-  
-  
-  
-  
-  
 }
--- src/app/shared/services/firebase.service.ts
+++ src/app/shared/services/firebase.service.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import {
   Database,
   ref,
@@ -6,10 +6,11 @@ import {
   get,
   Unsubscribe,
   push,
+  child,
 } from '@angular/fire/database';
 import { User } from '@angular/fire/auth';
-import { Observable, from, throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { Observable, combineLatest, from, of, throwError } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
 import { Channel } from '../interfaces/channel';
 
 @Injectable({
@@ -36,6 +37,7 @@ export class FirebaseService {
         uid: user.uid,
         displayName: user.displayName,
         email: user.email,
+        channelKeys: ['-OMCzsmmcro3xcrQMOVw'],
       })
     ).pipe(
       map(() => null),
@@ -45,7 +47,7 @@ export class FirebaseService {
       })
     );
   }
-
+  // -OMCzsmmcro3xcrQMOVw
   unsubscribeUserData(): void {
     if (this.unsubscribeUser) {
       this.unsubscribeUser();
@@ -57,7 +59,7 @@ export class FirebaseService {
     displayName: string;
     email: string;
     uid: string;
-    // channels: [];
+    channelKeys: string[];
   } | null> {
     if (!uid) {
       return throwError(
@@ -73,7 +75,7 @@ export class FirebaseService {
             displayName: string;
             email: string;
             uid: string;
-            // channels: [];
+            channelKeys: string[];
           };
         } else {
           return null;
@@ -128,12 +130,154 @@ export class FirebaseService {
           time: timestamp,
         },
       ],
+      private: false,
     };
 
     return from(set(newChannelRef, newChannel)).pipe(
       map(() => channelKey),
       catchError((error) => {
         console.error('Fehler beim Erstellen des Channels:', error);
+        return throwError(() => error);
+      })
+    );
+  }
+
+  getChannel(channelKey: string): Observable<Channel | null> {
+    if (!channelKey) {
+      return throwError(
+        () => new Error('getChannel: channelKey darf nicht leer sein.')
+      );
+    }
+
+    const dbRef = ref(this.database);
+
+    return from(get(child(dbRef, `channels/${channelKey}`))).pipe(
+      map((snapshot) => {
+        if (snapshot.exists()) {
+          return snapshot.val() as Channel;
+        } else {
+          return null;
+        }
+      }),
+      catchError((error) => {
+        console.error('Fehler beim Abrufen des Channels:', error);
+        return throwError(() => error);
+      })
+    );
+  }
+
+  getChannelsForUser(uid: string): Observable<Channel[]> {
+    if (!uid) {
+      return throwError(
+        () => new Error('getChannelsForUser: UID darf nicht leer sein.')
+      );
+    }
+
+    return this.getUserData(uid).pipe(
+      switchMap((userData) => {
+        if (
+          userData &&
+          userData.channelKeys &&
+          userData.channelKeys.length > 0
+        ) {
+          const channelObservables = userData.channelKeys.map((channelKey) =>
+            this.getChannel(channelKey)
+          );
+          return combineLatest(channelObservables).pipe(
+            map(
+              (channels) =>
+                channels.filter((channel) => channel !== null) as Channel[]
+            )
+          );
+        } else {
+          return of([]);
+        }
+      }),
+      catchError((error) => {
+        console.error(
+          'Fehler beim Abrufen der Kanäle für den Benutzer:',
+          error
+        );
+        return throwError(() => error);
+      })
+    );
+  }
+
+  getUserChannelKeys(uid: string): Observable<string[]> {
+    return this.getUserData(uid).pipe(
+      map((userData) => {
+        if (userData && userData.channelKeys) {
+          return userData.channelKeys;
+        } else {
+          return [];
+        }
+      }),
+      catchError((error) => {
+        console.error('Error getting user channel keys:', error);
+        return throwError(() => error);
+      })
+    );
+  }
+
+  addChannelKeyToUser(uid: string, channelKey: string): Observable<void> {
+    if (!uid || !channelKey) {
+      return throwError(
+        () =>
+          new Error('addChannelKeyToUser: uid and channelKey must be provided.')
+      );
+    }
+
+    const userRef = ref(this.database, `users/${uid}`);
+
+    return this.getUserData(uid).pipe(
+      switchMap((userData) => {
+        const currentKeys =
+          userData && userData.channelKeys ? userData.channelKeys : [];
+        if (currentKeys.includes(channelKey)) {
+          return of(null);
+        }
+
+        const updatedKeys: string[] = [...currentKeys, channelKey];
+        return from(set(child(userRef, 'channelKeys'), updatedKeys));
+      }),
+      map(() => {
+        return;
+      }),
+      catchError((error) => {
+        console.error('Error adding channel key to user:', error);
+        return throwError(() => error);
+      })
+    );
+  }
+
+  removeChannelKeyFromUser(uid: string, channelKey: string): Observable<void> {
+    if (!uid || !channelKey) {
+      return throwError(
+        () =>
+          new Error(
+            'removeChannelKeyFromUser: uid and channelKey must be provided'
+          )
+      );
+    }
+
+    const userRef = ref(this.database, `users/${uid}`);
+
+    return this.getUserData(uid).pipe(
+      switchMap((userData) => {
+        if (!userData || !userData.channelKeys) {
+          return of(null);
+        }
+
+        const updatedKeys = userData.channelKeys.filter(
+          (key) => key !== channelKey
+        );
+        return from(set(child(userRef, 'channelKeys'), updatedKeys));
+      }),
+      map(() => {
+        return;
+      }),
+      catchError((error) => {
+        console.error('Error removing channel key from user:', error);
         return throwError(() => error);
       })
     );
