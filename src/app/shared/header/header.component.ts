fc82ae0afe4ded097454b81ff4d7aa1e65f0dd44
Maximilian Wagener <159955254+DeVAiRMaX@users.noreply.github.com>
2025-04-09 20:26:40 +0200

    #searchfunction done

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,6 +6,7 @@ import { AuthService } from '../services/auth.service';
 import { User } from '../interfaces/user';
 import { Observable } from 'rxjs';
 import { AsyncPipe, NgIf } from '@angular/common';
+import { FirebaseService } from '../services/firebase.service';
 
 @Component({
   selector: 'app-header',
@@ -15,11 +16,59 @@ import { AsyncPipe, NgIf } from '@angular/common';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  searchValue: string = '';
+  searchResults: any[] = [];
+
   private authService: AuthService = inject(AuthService);
+  private databaseService: FirebaseService = inject(FirebaseService);
+
   user$: Observable<User | null>;
 
   constructor(public dialog: MatDialog) {
     this.user$ = this.authService.user$;
+  }
+
+  async onSearchChange(): Promise<void> {
+  
+    // Alle Daten aus der Datenbank abrufen
+    const data = await this.databaseService.getDatabaseData();
+  
+    if (this.searchValue.trim().length > 0) {
+      const lowerSearchTerm = this.searchValue.toLowerCase();
+  
+      const channels: any[] = data.channels 
+        ? Object.values(data.channels as Record<string, any>).filter((channel: any) =>
+            channel.channelName && channel.channelName.toLowerCase().includes(lowerSearchTerm)
+          )
+        : [];
+  
+      const users: any[] = data.users 
+        ? Object.values(data.users as Record<string, any>).filter((user: any) =>
+            user.displayName && user.displayName.toLowerCase().includes(lowerSearchTerm)
+          )
+        : [];
+      
+      // messages filtern (wenn vorhanden)
+      // const messages: any[] = data.messages 
+      //   ? Object.values(data.users as Record<string, any>).filter((user: any) =>
+      //       user.displayName && user.displayName.toLowerCase().includes(lowerSearchTerm)
+      //     )
+      //   : [];
+  
+      this.searchResults = [
+        ...channels.map((channel: any) => ({
+          type: 'Channel',
+          data: channel
+        })),
+        ...users.map((user: any) => ({
+          type: 'User',
+          data: user
+        }))
+      ];
+  
+    } else {
+      this.searchResults = [];
+    }
   }
 
   openDialog() {
